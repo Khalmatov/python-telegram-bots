354330acf53e5a0f959845eb269ec1308620d282
Aladdin <aladdin@pc>
2021-03-23 23:40:56 +0300

    неудачная попытка устновить webhook на localhost

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
+# Telegram-боты от А до Я
+
+### Начальная настройка
+
 ```bash
-	python3 -m venv venv
-	source ./venv/bin/activate
-	pip install pip --upgrade
-	pip install -r requirements.txt
+python3 -m venv venv
+source ./venv/bin/activate
+pip install pip --upgrade
+pip install -r requirements.txt
+```
+
+***
+
+### Бот с webhook на Flask
+
+```bash
+ssh -R 80:localhost:5000 localhost.run
+```
+
+#### Установка вебхука
+
+```url
+https://api.telegram.org/bot123445:mytoken/setWebhook?url=https://mysite.com:5000
+```
+
+
+В ответ вы должен получить такой ответ:
+```json
+{
+  "ok":true,
+  "result":true,
+  "description":"Webhook was set"
+}
 ```
--- bot-flask/bot.py
+++ bot-flask/bot.py
@@ -1,16 +1,40 @@
 from flask import Flask
-from flask_sslify import SSLify 
+from flask_sslify import SSLify
+from flask import request
+from flask import jsonify
+
+import requests
+import json
+from config import TOKEN
+
+app = Flask(__name__)  # __name__ - ссылка на текущий файл
+# sslify = SSLify(app)
+# раскомментируй сверху, если бот запущен:
+#		1) на хостинге с SSL-сертификатом
+#		2) или через туннель localhost.run
+URL = f'https://api.telegram.org/bot{TOKEN}/'  # стандартный url для общения с ботом
+
+def write_json(dictionary):
+	with open('answer.json', 'w') as file:
+		json.dump(dictionary, file, indent=2, ensure_ascii=False)
 
 
-app = Flask(__name__) # __name__ - ссылка на текущий файл
-# sslify = SSLify(app) # раскомментируй, если бот лежит на хостинге с SSL-сертификатом
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return('<h1>test flask</h1>')
+	"""
+	Функция запускается при поступлении запросов на корневой уровень
+	"""
 
+	if request.method == 'POST':
+		r = request.get_json()
+		write_json(r)
+
+		return jsonify(r)
+
+	elif request.method == 'GET':
+		return '<h1>Hello, i\'m a bot. Who are u?</h1>'
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run() # 127.0.0.1:5000
